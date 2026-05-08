bf29b8b0e3546e03180e1db2acf0879606bba7f7
Vinicio Monteiro <viniciomsilva@outlook.com.br>
2026-05-08 19:10:38 -0300

    refactor: implement score system, attribute tracking, and modular menu

--- super_trumps/super_trumps.c
+++ super_trumps/super_trumps.c
@@ -23,10 +23,17 @@ typedef struct Card {
     float ppt_dsty;  // population density
     unsigned int population;
     unsigned int attractions;  // tourist attractions
-    char city_name[CITY_NAME_SZ];
+    char name[CITY_NAME_SZ];
     char code[CODE_SZ];
     char state;
 } Card;
+
+typedef struct Attribute {
+    short used;
+    short lower;
+    short winner;
+    char label[30];
+} Attribute;
 
 void line(void) { printf("\n---\n\n"); }
 
@@ -55,8 +62,8 @@ void register_cards(Card* cds, int size) {
         cds[i].code[strcspn(cds[i].code, "\n")] = '\0';  // remove '\n'
 
         printf("Nome da cidade: ");
-        fgets(cds[i].city_name, sizeof(cds[i].city_name), stdin);
-        cds[i].city_name[strcspn(cds[i].city_name, "\n")] = '\0';
+        fgets(cds[i].name, sizeof(cds[i].name), stdin);
+        cds[i].name[strcspn(cds[i].name, "\n")] = '\0';
 
         printf("Populacao: ");
         fgets(buffer, sizeof(buffer), stdin);
@@ -107,7 +114,7 @@ void display_cards(Card* cds, int size) {
         printf("| Est: %c ", cds[i].state);
         printf("| Cod: %s ", cds[i].code);
         printf("| SUPER POWER: " FMT_DEC " \n", cds[i].super_power);
-        printf("Nome da cidade: %s \n", cds[i].city_name);
+        printf("Nome da cidade: %s \n", cds[i].name);
         printf("Populacao: %u hab. \n", cds[i].population);
         printf("Area: " FMT_DEC " km2 \n", cds[i].area);
         printf("PIB: R$ " FMT_DEC " bi \n", cds[i].gdp);
@@ -117,18 +124,209 @@ void display_cards(Card* cds, int size) {
     }
 }
 
-void duel(const char* attr, char* name1, double val1, char* name2, double val2, short lower) {
-    printf(">>> Atributo: %-24s ", attr);
+short compare(double val1, double val2, short lower) {
+    if (val1 == val2) return -1;
 
-    if (val1 == val2) {
-        printf("| EMPATE | Valor " FMT_DEC "\n", val1);
-        return;
+    return (lower && val1 > val2) || val1 < val2;
+}
+
+void display_res(char* attr, char* winner, double v_winner, char* loser, double v_loser, short draw) {
+    printf("\nResultado: %-18s | ", attr);
+    if (!draw) {
+        printf("Ganhador: %s [ ", winner);
+        printf(FMT_DEC " ] | ", v_winner);
+        printf("Perdedor: %s [ ", loser);
+        printf(FMT_DEC " ] \n", v_loser);
+    } else {
+        printf("%s | ", winner);
+        printf("%s | ", loser);
+        printf("EMPATE [ " FMT_DEC " ] \n", v_winner);
     }
+}
 
-    short winner = (lower && val1 > val2) || val1 < val2;
+void duel(Card* cds) {
+    Attribute attrs[7] = {
+        {.label = "Populacao", .used = 0, .winner = 0, .lower = 0},
+        {.label = "Area", .used = 0, .winner = 0, .lower = 0},
+        {.label = "Densidade", .used = 0, .winner = 0, .lower = 1},
+        {.label = "PIB", .used = 0, .winner = 0, .lower = 0},
+        {.label = "PIB Per Capita", .used = 0, .winner = 0, .lower = 0},
+        {.label = "Ponto Turisticos", .used = 0, .winner = 0, .lower = 0},
+        {.label = "SUPER POWER", .used = 0, .winner = 0, .lower = 0},
+    };
+    short o = 0;
+    double score_player_1 = .0;
+    double score_player_2 = .0;
+    char buffer[8];
 
-    printf("| Vencedor: %-24s ", (!winner) ? name1 : name2);  // !0 == gamer #1 wins
-    printf("| Valor: " FMT_DEC "\n", (!winner) ? val1 : val2);
+    do {
+        clean_terminal();
+
+        for (short i = 0; i < 7; i++) {
+            if (!attrs[i].used) printf("[ %d ] %s \n", (i + 1), attrs[i].label);
+        }
+
+        printf("[ 0 ] Resultado Final \n\n");
+        printf("Escolha > ");
+        fgets(buffer, sizeof(buffer), stdin);
+        o = (short)strtol(buffer, NULL, 10) - 1;
+
+        switch (o) {
+            case -1:
+                if (score_player_1 > score_player_2)
+                    display_res("Final", cds[0].name, score_player_1, cds[1].name, score_player_2, 0);
+                else if (score_player_2 > score_player_1)
+                    display_res("Final", cds[1].name, score_player_2, cds[0].name, score_player_1, 0);
+                else
+                    display_res("Final", cds[0].name, score_player_1, NULL, 0.0, 1);
+                break;
+
+            case 0:  // population duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare((double)cds[0].population, (double)cds[1].population, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += (double)cds[0].population;
+                    else if (attrs[o].winner)
+                        score_player_2 += (double)cds[1].population;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].population, cds[1].name,
+                                (double)cds[1].population, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, (double)cds[1].population, cds[0].name,
+                                (double)cds[0].population, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].population, NULL, 0.0, 1);
+                break;
+
+            case 1:  // area duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare((double)cds[0].area, (double)cds[1].area, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += (double)cds[0].area;
+                    else if (attrs[o].winner)
+                        score_player_2 += (double)cds[1].area;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].area, cds[1].name, (double)cds[1].area, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, (double)cds[1].area, cds[0].name, (double)cds[0].area, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].area, NULL, 0.0, 1);
+                break;
+
+            case 2:  // density duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare((double)cds[0].ppt_dsty, (double)cds[1].ppt_dsty, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += 1 / (double)cds[0].ppt_dsty;
+                    else if (attrs[o].winner)
+                        score_player_2 += 1 / (double)cds[1].ppt_dsty;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].ppt_dsty, cds[1].name,
+                                (double)cds[1].ppt_dsty, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, (double)cds[1].ppt_dsty, cds[0].name,
+                                (double)cds[0].ppt_dsty, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].ppt_dsty, NULL, 0.0, 1);
+                break;
+
+            case 3:  // gdp duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare(cds[0].gdp, cds[1].gdp, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += cds[0].gdp;
+                    else if (attrs[o].winner)
+                        score_player_2 += cds[1].gdp;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, cds[0].gdp, cds[1].name, cds[1].gdp, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, cds[1].gdp, cds[0].name, cds[0].gdp, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, cds[0].gdp, NULL, 0.0, 1);
+                break;
+
+            case 4:  // gdp per capita duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare(cds[0].gdp_pc, cds[1].gdp_pc, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += cds[0].gdp_pc;
+                    else if (attrs[o].winner)
+                        score_player_2 += cds[1].gdp_pc;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, cds[0].gdp_pc, cds[1].name, cds[1].gdp_pc, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, cds[1].gdp_pc, cds[0].name, cds[0].gdp_pc, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, cds[0].gdp_pc, NULL, 0.0, 1);
+                break;
+
+            case 5:  // attractions duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare((double)cds[0].attractions, (double)cds[1].attractions, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += (double)cds[0].attractions;
+                    else if (attrs[o].winner)
+                        score_player_2 += (double)cds[1].attractions;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].attractions, cds[1].name,
+                                (double)cds[1].attractions, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, (double)cds[1].attractions, cds[0].name,
+                                (double)cds[0].attractions, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, (double)cds[0].attractions, NULL, 0.0, 1);
+                break;
+
+            case 6:  // super_power duel
+                if (!attrs[o].used) {
+                    attrs[o].used = 1;
+                    attrs[o].winner = compare(cds[0].super_power, cds[1].super_power, attrs[o].lower);
+
+                    if (!attrs[o].winner)
+                        score_player_1 += cds[0].super_power;
+                    else if (attrs[o].winner)
+                        score_player_2 += cds[1].super_power;
+                }
+
+                if (!attrs[o].winner)
+                    display_res(attrs[o].label, cds[0].name, cds[0].super_power, cds[1].name, cds[1].super_power, 0);
+                else if (attrs[o].winner)
+                    display_res(attrs[o].label, cds[1].name, cds[1].super_power, cds[0].name, cds[0].super_power, 0);
+                else
+                    display_res(attrs[o].label, cds[0].name, cds[0].super_power, NULL, 0.0, 1);
+                break;
+
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+
+        pause();
+    } while (o >= 0);
 }
 
 void main_menu(void) {
@@ -143,14 +341,14 @@ void main_menu(void) {
 int main(void) {
     Card cds[CARDS_SZ];
     char buffer[BUFFER_SZ];
-    unsigned short opt = 0;
+    short opt = 0;
 
     main_menu();
 
     do {
         printf("Escolha > ");
         fgets(buffer, sizeof(buffer), stdin);
-        opt = (unsigned short)strtoul(buffer, NULL, 10);
+        opt = (short)strtoul(buffer, NULL, 10);
 
         switch (opt) {
             case 1:
@@ -163,38 +361,7 @@ int main(void) {
                 break;
 
             case 3:
-                clean_terminal();
-
-                short i = 0;
-                short j = 1;
-
-                // population duel
-                duel("Populacao", cds[i].city_name, (double)cds[i].population, cds[j].city_name,
-                     (double)cds[j].population, 0);
-
-                // area duel
-                duel("Area", cds[i].city_name, cds[i].area, cds[j].city_name, cds[j].area, 0);
-
-                // population density duel
-                duel("Densidade Populacional", cds[i].city_name, cds[i].ppt_dsty, cds[j].city_name,
-                     cds[j].ppt_dsty, 1);
-
-                // gdp duel
-                duel("PIB", cds[i].city_name, cds[i].gdp, cds[j].city_name, cds[j].gdp, 0);
-
-                // gpd per capita duel
-                duel("PIB Per Capita", cds[i].city_name, cds[i].gdp_pc, cds[j].city_name,
-                     cds[j].gdp_pc, 0);
-
-                // tourist attractions duel
-                duel("Pontos Turisticos", cds[i].city_name, (double)cds[i].attractions,
-                     cds[j].city_name, (double)cds[j].attractions, 0);
-
-                // super power duel
-                duel("SUPER POWER", cds[i].city_name, cds[i].super_power, cds[j].city_name,
-                     cds[j].super_power, 0);
-
-                pause();
+                duel(cds);
                 main_menu();
                 break;
 
