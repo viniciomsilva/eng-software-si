d775f527e8b9d607eaf25fef1980898aaca11bee
Vinicio Monteiro <viniciomsilva@outlook.com.br>
2026-05-10 21:34:34 -0300

    refactor: encapsulate piece initialization and chessboard setup into structured functions

--- chess_move/chess_move.c
+++ chess_move/chess_move.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+#define PIECES_LEN 16
 #define CB_LEN 8
 
 const char COLUMNS[CB_LEN] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
+
+typedef struct Piece {
+    short x;
+    short y;
+    char label[4];
+} Piece;
+
+void start(char* cb[CB_LEN][CB_LEN], Piece* pcs) {
+    for (short i = 0; i < PIECES_LEN; i++) cb[pcs[i].y][pcs[i].x] = pcs[i].label;
+}
 
 void chessboard(char* cb[CB_LEN][CB_LEN]) {
     /*
@@ -32,27 +43,16 @@ void chessboard(char* cb[CB_LEN][CB_LEN]) {
 
 int main(void) {
     char* cb[CB_LEN][CB_LEN] = {NULL};
+    Piece pieces[PIECES_LEN] = {
+        {.label = "PA1", .x = 0, .y = 6}, {.label = "PA2", .x = 1, .y = 6}, {.label = "PA3", .x = 2, .y = 6},
+        {.label = "PA4", .x = 3, .y = 6}, {.label = "PA5", .x = 4, .y = 6}, {.label = "PA6", .x = 5, .y = 6},
+        {.label = "PA7", .x = 6, .y = 6}, {.label = "PA8", .x = 7, .y = 6}, {.label = "RK1", .x = 0, .y = 7},
+        {.label = "KN1", .x = 1, .y = 7}, {.label = "BI1", .x = 2, .y = 7}, {.label = "QEN", .x = 3, .y = 7},
+        {.label = "KNG", .x = 4, .y = 7}, {.label = "BI2", .x = 5, .y = 7}, {.label = "KN2", .x = 6, .y = 7},
+        {.label = "RK2", .x = 7, .y = 7},
+    };
 
-    // 2 - PA1 | PA2 | PA3 | PA4 | PA5 | PA6 | PA7 | PA8
-    cb[6][0] = "PA1";
-    cb[6][1] = "PA2";
-    cb[6][2] = "PA3";
-    cb[6][3] = "PA4";
-    cb[6][4] = "PA5";
-    cb[6][5] = "PA6";
-    cb[6][6] = "PA7";
-    cb[6][7] = "PA8";
-
-    // 1 - RK1 | KN1 | BI1 | QEN | KNG | BI2 | KN2 | RK2
-    cb[7][0] = "RK1";
-    cb[7][1] = "KN1";
-    cb[7][2] = "BI1";
-    cb[7][3] = "QEN";
-    cb[7][4] = "KNG";
-    cb[7][5] = "BI2";
-    cb[7][6] = "KN2";
-    cb[7][7] = "RK2";
-
+    start(cb, pieces);
     chessboard(cb);
 
     return 0;
