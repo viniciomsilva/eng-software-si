662c8ad19d7098d445a6a572a91a989f81173661
Vinicio Monteiro <viniciomsilva@outlook.com.br>
2026-05-25 21:56:06 -0300

    refactor: rename draw_line to draw_div and reorganize output functions for clarity

--- battleship/src/battleship_io.c
+++ battleship/src/battleship_io.c
@@ -4,10 +4,12 @@
 
 #include "battleship.h"
 
+#define LAST_POS_BOARD (BOARD_SZ - 1)
+
 const char COLUMNS[BOARD_SZ] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' };
 
 // Auxiliar functions
-void draw_line(char c, int len, const char* start, const char* end) {
+void draw_div(char c, int len, const char* start, const char* end) {
     printf("\n%s", start);
 
     while (len > 0) {
@@ -18,29 +20,42 @@ void draw_line(char c, int len, const char* start, const char* end) {
     printf("%s\n", end);
 }
 
-// Output functions
-void draw_board(char (*draw_board)[BOARD_SZ]) {
+void draw_columns_letters() {
     printf("   |");
 
     for (int i = 0; i < BOARD_SZ; i++) {
         printf("  %c  |", COLUMNS[i]);
     }
+}
 
-    draw_line('-', 59, "   +", "+");
+void draw_cell(char content, int x) {
+    printf("  %c  ", content ? content : ' ');
 
+    if (x != LAST_POS_BOARD) {
+        putchar(' ');
+    }
+}
+
+void draw_lines(char (*board)[BOARD_SZ]) {
     for (int y = 0; y < BOARD_SZ; y++) {
         printf("%2d |", (y + 1));
 
         for (int x = 0; x < BOARD_SZ; x++) {
-            char content = draw_board[y][x];
-
-            printf("  %c  |", content ? content : ' ');
+            draw_cell(board[y][x], x);
         }
 
-        if (y != BOARD_SZ - 1) {
-            draw_line(' ', 59, "   |", "|");
+        putchar('|');
+
+        if (y != LAST_POS_BOARD) {
+            draw_div(' ', 59, "   |", "|");
         }
     }
+}
 
-    draw_line('-', 59, "   +", "+");
+// Output functions
+void draw_board(char (*board)[BOARD_SZ]) {
+    draw_columns_letters();
+    draw_div('-', 59, "   +", "+");
+    draw_lines(board);
+    draw_div('-', 59, "   +", "+");
 }
